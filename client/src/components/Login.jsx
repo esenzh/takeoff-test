4faa9199a858340b98da1acbbf8b1c9d969b3685
Esen Zheenchoroev <stefunices@gmail.com>
2020-07-25 21:45:27 +0300

    feat: handle the login on client side

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,16 +1,36 @@
 import React, { useState } from 'react';
 import LoginButton from './Button';
+import { useHistory } from 'react-router-dom';
 
 function Login() {
-    const [emailInput, setEmailInput] = useState('');
-    const [passwordInput, setPasswordInput] = useState('');
+    const [email, setEmailInput] = useState('');
+    const [password, setPasswordInput] = useState('');
+
+    const history = useHistory();
+
     const handleOnChange = event => {
         event.target.name === 'email'
             ? setEmailInput(event.target.value)
             : setPasswordInput(event.target.value)
     }
+
+    const handleLogin = async event => {
+        event.preventDefault();
+        const response = await fetch('/api/auth/login', {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ email, password })
+        })
+        const result = await response.json();
+        if (result.message === 'Access granted') {
+            history.push('/')
+        } else {
+            alert(result.message)
+        }
+    }
+
     return (
-        <form >
+        <form onSubmit={handleLogin}>
             <label htmlFor="GET-email">Email</label>
             <input id='GET-email' type="email" name='email' onChange={handleOnChange} required />
 
