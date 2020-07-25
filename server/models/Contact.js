0e956625f36fcd2ce13361369ce8487e5b35d93a
Esen Zheenchoroev <stefunices@gmail.com>
2020-07-25 18:18:12 +0300

    feat: contact routes get all, add, update and delete

--- server/models/Contact.js
+++ server/models/Contact.js
@@ -2,7 +2,7 @@ const { Schema, model } = require("mongoose");
 
 const schema = new Schema({
   name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
+  email: { type: String, required: true },
   phone: { type: String, required: true},
   author: {type: Schema.Types.ObjectId, ref: "User"}
 });
--- server/routes/contact.route.js
+++ server/routes/contact.route.js
@@ -5,6 +5,13 @@ const router = Router();
 
 router
   .get("/contacts", async (req, res) => {
+    try {
+      const userID = req.session.user._id;
+      const contacts = await Contact.find({ author: userID });
+      res.status(200).json({ contacts });
+    } catch (e) {
+      res.status(500).json({ message: "Something went wrong, try again" });
+    }
     res.json({ message: "ALL posts" });
   })
   .post("/contacts", async (req, res) => {
@@ -14,7 +21,25 @@ router
 
       const contact = new Contact({ name, email, phone, author: user._id });
       await contact.save();
-      res.status(201).json({ message: "Contact created" });
+      res.status(201).json({ message: "Contact is created" });
+    } catch (e) {
+      res.status(500).json({ message: "Something went wrong, try again" });
+    }
+  })
+  .put("contacts", async (req, res) => {
+    try {
+      const { id, name, email, phone } = req.body;
+      await Contact.updateOne({ _id: id }, { $set: { name, email, phone } });
+      res.status(200).json({ message: "Contact is updated" });
+    } catch (e) {
+      res.status(500).json({ message: "Something went wrong, try again" });
+    }
+  })
+  .delete("contacts", async (req, res) => {
+    try {
+      const { id } = req.body;
+      await Contact.deleteOne({ _id: id });
+      res.status(200).json({ message: "Contact deleted" });
     } catch (e) {
       res.status(500).json({ message: "Something went wrong, try again" });
     }
