6efa31bb9c1115d1a22a91c8f470e64a22d6902b
Esen Zheenchoroev <stefunices@gmail.com>
2020-07-26 13:24:56 +0300

    refactor: send back the added contact with given id

--- server/routes/contact.route.js
+++ server/routes/contact.route.js
@@ -21,7 +21,7 @@ router
 
       const contact = new Contact({ name, email, phone, author: user._id });
       await contact.save();
-      res.status(201).json({ message: "Contact is created" });
+      res.status(201).json({ message: "Contact is created", contact });
     } catch (e) {
       res.status(500).json({ message: "Something went wrong, try again" });
     }
